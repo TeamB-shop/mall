355f7808d1d23acc31577d9b01222e4c2b8567c9
SeonikKim <tjsdlrdl122@naver.com>
2025-03-14 10:13:20 +0900

    수정2 //

--- mall/src/main/java/admin/idcheck.java
+++ mall/src/main/java/admin/idcheck.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.m_dbinfo;
-
+//ㅎㅎㅎㅎ
 public class idcheck extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	PrintWriter pw = null;
