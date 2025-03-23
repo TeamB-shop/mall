cc877fc8ad34179b374adfbe23d425d21ddc1078
tjsdl <tjsdlrdl122@naver.com>
2025-03-23 21:05:41 +0900

    view에서 삭제 오류 수정

--- mall/src/main/java/notice/notice_delete.java
+++ mall/src/main/java/notice/notice_delete.java
@@ -14,39 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 import model.m_dbinfo;
 
 
-//public class notice_delete extends HttpServlet {
-//	private static final long serialVersionUID = 1L;
-//
-//	protected void doGet(HttpServletRequest rq, HttpServletResponse rp) throws ServletException, IOException {
-//        rp.setContentType("text/html; charset=utf-8");
-//        
-//		String midx = rq.getParameter("midx");
-//	    Connection con = null;
-//	    PreparedStatement ps = null;
-//	    m_dbinfo db = new m_dbinfo();
-//	    int result = 0;
-//
-//        try {
-//            con = db.getConnection();
-//            String sql = "delete from notice where midx = ?";
-//            ps = con.prepareStatement(sql);
-//            ps.setString(1, midx);
-//            result = ps.executeUpdate();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            try { if (ps != null) ps.close(); } catch (Exception e) {}
-//            try { if (con != null) con.close(); } catch (Exception e) {}
-//        }
-//
-//        PrintWriter pw = rp.getWriter();
-//        if (result > 0) {
-//            pw.write("<script>alert('삭제 완료'); location.href='./notice_list.do';</script>");
-//        } else {
-//            pw.write("<script>alert('삭제 실패'); history.go(-1);</script>");
-//        }
-//    }
-//}
 
 public class notice_delete extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -54,8 +21,18 @@ public class notice_delete extends HttpServlet {
     protected void doGet(HttpServletRequest rq, HttpServletResponse rp) throws ServletException, IOException {
         rp.setContentType("text/html; charset=utf-8");
 
-        // JSP에서 전달한 체크박스 값은 "ids"라는 이름으로 여러 값이 전달됩니다.
+        rp.setContentType("text/html; charset=utf-8");
+
         String[] ids = rq.getParameterValues("ids");
+
+        // 단건 삭제
+        if (ids == null) {
+            String midx = rq.getParameter("midx");
+            if (midx != null && !midx.trim().equals("")) {
+                ids = new String[] { midx };
+            }
+        }
+
         Connection con = null;
         PreparedStatement ps = null;
         m_dbinfo db = new m_dbinfo();
@@ -86,6 +63,4 @@ public class notice_delete extends HttpServlet {
             pw.write("<script>alert('삭제 실패'); history.go(-1);</script>");
         }
     }
-    
-
 }
